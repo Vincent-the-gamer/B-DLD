3c2c3ca8e8c652428333f13a43b96cc4e7533092
Vincent-the-gamer <1127620453@qq.com>
2022-09-12 20:27:08 +0800

    首次提交

---
+++ src/components/UserModal/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <a-modal
+    v-model:visible="visible"
+    :closable="true"
+    :footer="null">
+    <div class="user fc ac">
+      <img src="../../assets/images/user.png" alt="" class="avatar">
+      <div class="version mt16">
+        {{ `${projectName} - v${version}` }} <ReloadOutlined @click="checkUpdate.checkUpdate()" />
+      </div>
+      <div class="git mt16">用的时候收敛点</div>
+    </div>
+  </a-modal>
+  <CheckUpdate ref="checkUpdate" />
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { ReloadOutlined } from '@ant-design/icons-vue'
+import CheckUpdate from '../CheckUpdate/index.vue'
+
+const checkUpdate = ref<any>(null)
+const packageInfo = require('../../../package.json')
+const visible = ref<boolean>(false)
+const projectName = "Bilibili视频下载器"
+const version = ref<string>(packageInfo.version)
+const projectUrl = ref<string>(packageInfo.homepage)
+
+const toogleVisible = () => {
+  visible.value = !visible.value
+}
+const openBrowser = (url: string):void => {
+  window.electron.openBrowser(url)
+}
+defineExpose({
+  toogleVisible
+})
+</script>
+
+<style scoped lang="less">
+.user{
+  .avatar{
+    width: 150px;
+    border: 1px solid #eeeeee;
+    border-radius: 50%;
+  }
+}
+</style>
